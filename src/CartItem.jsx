efa14a46246ad55515ece5045372032eb3076049
daquillen9826 <daquillen9826@comcast.net>
2025-07-15 00:50:51 -0400

    fixed calculateTotalAmount

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,7 +8,7 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = (cart) => {
+  const calculateTotalAmount = () => {
     let total = 0;
     cart.forEach((item) => {
         total += parseFloat(item.cost.substring(1)) * item.quantity;
